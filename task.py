381343836c1826bcec1d33f48c9a46bd739ecb9b
Griffin Adams <ga@answer.ai>
2024-06-18 21:15:20 +0000

    Add TriviaQA.

--- task.py
+++ task.py
@@ -99,11 +99,11 @@ class EvaluationTask(ABC):
 class Squality(EvaluationTask):
     DEFAULT_PROMPT_TEMPLATE = """You are given a story and a question. Answer the question in a paragraph.
 
-    Story:
-    {story}
+Story:
+{story}
 
-    Question:
-    {question}"""
+Question:
+{question}"""
 
     def __init__(
         self, prompt_template=DEFAULT_PROMPT_TEMPLATE, max_tokens=1024, **kwargs
@@ -137,7 +137,69 @@ class Squality(EvaluationTask):
         return out
 
 
-TASK_MAPPING = {"squality": Squality}
+class TriviaQA(EvaluationTask):
+    DEFAULT_PROMPT_TEMPLATE = """You are given a question and potentially relevant context from Wikipedia. Answer the question without any explanation.
+
+Context:
+{context}
+
+Question:
+{question}"""
+
+    def __init__(
+        self, prompt_template=DEFAULT_PROMPT_TEMPLATE, max_tokens=1024, **kwargs
+    ):
+        self.use_web = kwargs.pop("use_web", False)
+
+        super().__init__(
+            prompt_template, max_tokens, hf_args=["trivia_qa", "rc"], **kwargs
+        )
+
+        self.metrics = {
+            "BertScore": AutoMetric.from_name("bertscore"),
+            "Rouge": AutoMetric.from_name("rouge"),
+        }
+
+    def prepare_row(self, row: dict):
+        wikis = row["entity_pages"]
+        webs = row["search_results"]
+
+        wiki_n = len(wikis["title"])
+        web_n = len(webs["title"])
+
+        contexts = []
+
+        for i in range(wiki_n):
+            contexts.append("# " + wikis["title"][i] + "\n" + wikis["wiki_context"][i])
+
+        if self.use_web:
+            for j in range(web_n):
+                contexts.append(
+                    "# "
+                    + webs["title"][j]
+                    + "\n"
+                    + webs["description"][j]
+                    + "\n"
+                    + webs["search_context"][j]
+                )
+
+        context_str = "\n\n".join(contexts)
+        question = row["question"]
+        labels = row["answer"]["aliases"]
+        if row["answer"]["value"] not in labels:
+            labels.append(row["answer"]["value"])
+        assert len(labels) > 0
+        return {
+            "context": context_str,
+            "question": question,
+            "prompt": self.prompt_template.format(
+                context=context_str, question=question
+            ),
+            "labels": labels,
+        }
+
+
+TASK_MAPPING = {"squality": Squality, "triviaqa": TriviaQA}
 
 
 class AutoTask:
@@ -152,3 +214,42 @@ class AutoTask:
                 f"Task {task_name} not found. Available tasks: {TASK_MAPPING.keys()}"
             )
         return TASK_MAPPING[task_name](**kwargs)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser("Test out implementation of EvaluationTask")
+
+    parser.add_argument(
+        "--task", type=str, default="triviaqa", choices=TASK_MAPPING.keys()
+    )
+
+    args = parser.parse_args()
+
+    task = AutoTask.from_name(args.task)
+
+    test = task.get_test()
+
+    print("Example test datapoint:\n\n")
+    ex = test[0]
+    for k, v in ex.items():
+        print(f"{k}:\n{v}\n\n")
+
+    train_predictions = ["This is a train prediction"] * len(task.dataset["train"])
+    test_predictions = ["This is a test prediction"] * len(test)
+
+    print("A 'not ready' error should be displayed below:\n\n")
+    try:
+        task.train_metrics(predictions=train_predictions)
+    except Exception as e:
+        print(e)
+
+    print("A 'length mismatch' error should be displayed below:\n\n")
+    try:
+        task.test_metrics(predictions=test_predictions[:-1])
+    except Exception as e:
+        print(e)
+
+    print("Dummy metrics for test split:\n\n")
+    print(task.test_metrics(predictions=test_predictions))
