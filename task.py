a4ecbbaaa8534f449270ab26629465296148d500
Griffin Adams <ga@answer.ai>
2024-06-18 20:34:10 +0000

    Update Tasks to allow for train, val, test splits to be used. - This is necessary for training models outside of eval.py

--- task.py
+++ task.py
@@ -1,21 +1,99 @@
+from abc import ABC, abstractmethod
 from datasets import load_dataset
 from metric import AutoMetric
 
 
-class EvaluationTask:
-    def __init__(self, prompt_template, max_tokens, **kwargs):
+class EvaluationTask(ABC):
+    train_split: str = "train"
+    validation_split: str = "validation"
+    test_split: str = "test"
+    mandatory_cols = ["context", "question", "prompt", "labels"]
+
+    def __init__(self, prompt_template, max_tokens, hf_args=None, **kwargs):
         self.prompt_template = prompt_template
         self.max_tokens = max_tokens
-        self._load_data(**kwargs)
+        self.hf_args = hf_args
 
-    def _load_data(self):
-        raise NotImplementedError("This method should be overridden by subclasses.")
+        # Download the dataset
+        self._download(**kwargs)
 
-    def get_eval_prompts(self):
-        raise NotImplementedError("This method should be overridden by subclasses.")
+        # Lazy process each split as needed
+        self.is_ready = {
+            self.train_split: False,
+            self.validation_split: False,
+            self.test_split: False,
+        }
 
-    def compute_metrics(self, predictions):
-        raise NotImplementedError("This method should be overridden by subclasses.")
+    def _download(self):
+        # Can over-write if not using HF
+        self.dataset = load_dataset(*self.hf_args)
+
+    def get_split(self, split):
+        remove_cols = [
+            col
+            for col in self.dataset[split].column_names
+            if col not in self.mandatory_cols
+        ]
+        if not self.is_ready[split]:
+            self.dataset[split] = self.dataset[split].map(
+                self.prepare_batch, batched=True, remove_columns=remove_cols
+            )
+        self.is_ready[split] = True
+        return self.dataset[split]
+
+    def get_train(self):
+        return self.get_split(self.train_split)
+
+    def get_validation(self):
+        return self.get_split(self.validation_split)
+
+    def get_test(self):
+        return self.get_split(self.test_split)
+
+    def compute_metrics(self, predictions, split, dataset):
+        assert self.is_ready[split], f"Split {split} has not been processed yet."
+        assert (
+            len(dataset) == len(predictions)
+        ), f"Number of predictions and labels must match ({len(predictions)} != {len(dataset)})."
+        return self._compute_metrics(predictions, dataset)
+
+    def _compute_metrics(self, predictions: list, labels: list[str | list[str]]):
+        return {
+            metric_name: metric.compute(predictions, labels)
+            for metric_name, metric in self.metrics.items()
+        }
+
+    def train_metrics(self, predictions):
+        return self.compute_metrics(predictions, self.train_split, self.get_train())
+
+    def validation_metrics(self, predictions):
+        return self.compute_metrics(
+            predictions, self.validation_split, self.get_validation()
+        )
+
+    def test_metrics(self, predictions):
+        return self.compute_metrics(predictions, self.test_split, self.get_test())
+
+    def prepare_batch(self, batch):
+        keys = list(batch.keys())
+        n = len(batch[keys[0]])
+        processed = {k: [] for k in self.mandatory_cols}
+        for i in range(n):
+            row = {k: v[i] for k, v in batch.items()}
+            out = {k: None for k in self.mandatory_cols}
+            out = self.prepare_row(row)
+            # Most tasks will return a single dictionary example from a single row
+            if type(out) != list:
+                out = [out]
+            for x in out:
+                for k in self.mandatory_cols:
+                    processed[k].append(x.get(k, None))
+        return processed
+
+    @abstractmethod
+    def prepare_row(self, row) -> dict | list[dict]:
+        """Process a single row from the dataset."""
+        pass
 
 
 class Squality(EvaluationTask):
@@ -30,38 +108,33 @@ class Squality(EvaluationTask):
     def __init__(
         self, prompt_template=DEFAULT_PROMPT_TEMPLATE, max_tokens=1024, **kwargs
     ):
-        super().__init__(prompt_template, max_tokens, **kwargs)
+        super().__init__(
+            prompt_template, max_tokens, hf_args=["pszemraj/SQuALITY-v1.3"], **kwargs
+        )
 
-    def _load_data(self):
-        self.dataset = load_dataset("pszemraj/SQuALITY-v1.3")["test"]
-        eval_prompts = []
-        references = []
-        for datum in self.dataset:
-            story = datum["document"]
-            for question in datum["questions"]:
-                eval_prompts.append(
-                    self.prompt_template.format(
-                        story=story, question=question["question_text"]
-                    )
-                )
-                references.append(
-                    [resp["response_text"] for resp in question["responses"]]
-                )
-        self.eval_prompts = eval_prompts
-        self.references = references
         self.metrics = {
             "BertScore": AutoMetric.from_name("bertscore"),
             "Rouge": AutoMetric.from_name("rouge"),
         }
 
-    def get_eval_prompts(self):
-        return self.eval_prompts
-
-    def compute_metrics(self, predictions):
-        return {
-            metric_name: metric.compute(predictions, self.references)
-            for metric_name, metric in self.metrics.items()
-        }
+    def prepare_row(self, row: dict):
+        story = row["document"].strip()
+        questions = row["questions"]
+        out = []
+        for question in questions:
+            question_text = question["question_text"].strip()
+            prompt = self.prompt_template.format(
+                story=story, question=question["question_text"]
+            )
+            labels = [resp["response_text"].strip() for resp in question["responses"]]
+            out_row = {
+                "prompt": prompt,
+                "context": story,
+                "question": question_text,
+                "labels": labels,
+            }
+            out.append(out_row)
+        return out
 
 
 TASK_MAPPING = {"squality": Squality}
