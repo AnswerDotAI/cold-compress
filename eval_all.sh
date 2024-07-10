a68800be281ee26be24b6a5d11bc653249caad1f
root <fl@answer.ai>
2024-07-10 18:02:30 +0000

    Changing eval script to use compression ratio rather than fixed cache sizes.

--- eval_all.sh
+++ eval_all.sh
@@ -1,6 +1,13 @@
 python parallelize_evals.py \
 	--config_names random l2 scissor window \
-	--tasks truthfulqa rulerqa rulerniah rulervt rulercwe scrollsquality dolomites musique squality qmsum \
-	--cache_sizes 8192 4096 2048 1024 512 256 128 \
+	--tasks truthfulqa rulerqa rulerniah rulervt rulercwe scrollsquality musique squality dolomites qmsum reprobench \
+	--cache_sizes 0.75 0.5 0.25 0.1 0.05 \
+	--num_samples 100 \
+	--checkpoint_path ./checkpoints/Qwen/Qwen2-1.5B-Instruct/model.pth
+
+python parallelize_evals.py \
+	--config_names full \
+	--tasks truthfulqa rulerqa rulerniah rulervt rulercwe scrollsquality musique squality dolomites qmsum reprobench \
+	--cache_sizes 1.0 \
 	--num_samples 100 \
 	--checkpoint_path ./checkpoints/Qwen/Qwen2-1.5B-Instruct/model.pth
