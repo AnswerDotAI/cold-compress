c03248377c8d12f497bea2a299671bf3a46134fc
Griffin Adams <ga@answer.ai>
2024-07-13 20:12:35 +0000

    Minor change.

--- analysis/attention_loss_experiment.sh
+++ analysis/attention_loss_experiment.sh
@@ -5,11 +5,12 @@ set -e
 DIR=$(dirname $(dirname "$0"))
 export CKPT=$DIR/checkpoints/Qwen/Qwen2-1.5B-Instruct/model.pth
 RATIOS=(0.25 0.5 0.75)
+GLOBAL_TOKENS=4
 
 # For loop over ratios
 for RATIO in $RATIOS
 do
     echo "Running Attention Loss Experiments for ${CKPT} at Ratio ${RATIO}"
-    python eval.py --compile --tasks pg19 --checkpoint_path $CKPT --cache_strategy scissor --prompt_compression_strategy snapkv --max_cache_length $RATIO
-    python eval.py --compile --tasks pg19 --checkpoint_path $CKPT --cache_strategy debug_scissor --prompt_compression_strategy snapkv --max_cache_length $RATIO
+    python eval.py --compile --tasks pg19 --global_tokens $GLOBAL_TOKENS --checkpoint_path $CKPT --cache_strategy scissor --prompt_compression_strategy snapkv --max_cache_length $RATIO
+    python eval.py --compile --tasks pg19 --global_tokens $GLOBAL_TOKENS --checkpoint_path $CKPT --cache_strategy debug_scissor --prompt_compression_strategy snapkv --max_cache_length $RATIO
 done
