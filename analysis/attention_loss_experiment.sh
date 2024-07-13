4c297ffb5d8adf6a342d297e84d2059e73e008fa
Griffin Adams <ga@answer.ai>
2024-07-13 19:26:54 +0000

    Add attention loss experiment.

---
+++ analysis/attention_loss_experiment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+export CKPT=/workspace/context-compression/checkpoints/Qwen/Qwen2-1.5B-Instruct/model.pth
+RATIOS=(0.25 0.5 0.75)
+
+# For loop over ratios
+for RATIO in $RATIOS
+do
+    echo "Running Attention Loss Experiments for ${CKPT} at Ratio ${RATIO}"
+    python eval.py --compile --tasks pg19 --checkpoint_path $CKPT --cache_strategy scissor --prompt_compression_strategy snapkv --max_cache_length $RATIO
+    python eval.py --compile --tasks pg19 --checkpoint_path $CKPT --cache_strategy debug_scissor --prompt_compression_strategy snapkv --max_cache_length $RATIO
+done
