9aa83ef28ee3d38599df1d4e3512a15d08b822c8
Griffin Adams <ga@answer.ai>
2024-07-23 18:43:27 +0000

    Fix generate.py.

--- generate.py
+++ generate.py
@@ -113,10 +113,11 @@ def main(
     device_sync(device=device)  # MKG
 
     max_prompt_length, max_seq_length = compute_max_seq_length(
-        model, inputs, max_new_tokens
+        model, inputs, None, max_new_tokens
     )
     max_new_tokens = min(max_new_tokens, max_seq_length - max_prompt_length)
     setup_caches(model, tokenizer, inputs[0].device, max_seq_length, cache_kwargs)
+
     t0 = time.perf_counter()
 
     if (not profile) or (use_tp and rank != 0):
@@ -180,8 +181,6 @@ if __name__ == "__main__":
         default=None,
         help="Name of YAML file in ./cache_configs.",
     )
-
-    args = parser.parse_args()
 
     add_generation_arguments(parser)
     add_cache_arguments(parser)
